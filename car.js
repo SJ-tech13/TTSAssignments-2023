6c423e8c316e860edb91d2679af0a133357aefb0
Swapna Jha <45579175+SJ-tech13@users.noreply.github.com>
2023-06-02 00:00:49 -0400

    Add files via upload

---
+++ car.js
@@ -0,0 +1,19 @@
+let cups = null;
+let input ="";
+let input2="";
+let input3="";
+if(input){
+    console.log('true');
+}else{
+
+}
+if(input && input2 && input3){
+    SubmitEvent(Button);
+}else{
+    console.log('some information is missing to submit that sheet');
+}
+if(false){
+    console.log("Yes I think i need an oil change");
+}else{
+    console.log("My car doesnot need an oil change yet.");
+}
---
+++ code.js
@@ -0,0 +1,22 @@
+
+
+let cups = null;
+let input ="";
+let input2="";
+let input3="";
+if(input){
+    console.log('true');
+}else{
+
+}
+if(input && input2 && input3){
+    SubmitEvent(Button);
+}else{
+    console.log('some information is missing to submit that sheet');
+}
+if(false){
+    console.log("Yes I'll take another cup of tea");
+}else{
+    console.log("I think I'm okay for now");
+}
+
